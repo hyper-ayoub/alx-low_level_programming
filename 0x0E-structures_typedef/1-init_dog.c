dca07b300379528a2c4036b657bedfe00ce95103
hyper-ayoub <bouagnaayoub@gmail.com>
2023-08-16 15:53:24 -0700

    a

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * init_dog - initializes a dog 
+ * @d: the dog to init 
+ * @name: the dog's name
+ * @owner: owner's name
+ *
+ * Return: void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+         if (d)
+         {
+                 d->name = name;
+                 d->age = age;
+                 d->owner = owner;
+          }
+}
