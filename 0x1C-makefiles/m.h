83c377b5e7557037fa3f23c8c47d98d51336dacf
hyperayoub <bouagnaayoub@gmail.com>
2023-12-21 12:39:25 -0800

    Makefiles

---
+++ 0x1C-makefiles/m.h
@@ -0,0 +1,7 @@
+#ifndef M_H
+#define M_H
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_school(void);
+#endif
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "m.h"
+/*
+ * print_school
+ *
+ * Return: No value
+ */
+int main (void)
+{
+	print_school();
+	return (EXIT_SUCCESS);
+}
