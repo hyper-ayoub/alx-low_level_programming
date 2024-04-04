3bee75a9baf9134417c83d67b7bae0523e113581
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-04 08:33:02 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_numbers - function that print numbers
+ * print_numbers - function that prints numbers
  * Return: NULL
  */
-
 void print_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-putchar(c);
-}
-putchar('\n');
+    char c;
+    for (c = '0'; c <= '9'; c++)
+    {
+        putchar(c);
+    }
+    putchar('\n');
 }
