c5a597bb2d3e6b63c31fa07e6b2ea322e0eed103
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-04-04 08:34:39 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
 
 void print_numbers(void)
 {
-    char c;
-    for (c = '0'; c <= '9'; c++)
-    {
-        putchar(c);
-    }
-    putchar('\n');
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+putchar('\n');
 }
