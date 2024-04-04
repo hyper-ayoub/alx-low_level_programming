44195ed0a1f9056d6bd4acc1d3bdbac657cb7adb
hyperayoub <bouagnaayoub@gmail.com>
2024-04-04 00:30:30 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - function that print numbers
+ * Return: NULL
+ */
+
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++)
+_putchar(c);
+}
+_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int _isupper(int c);
 int _isdigit(int c);
